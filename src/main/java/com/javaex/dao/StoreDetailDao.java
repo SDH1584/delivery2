cffb0d120d277a8ed3036e77e208e20be90d8ca4
KYS <kyseul513@hanmail.net>
2022-03-07 18:09:33 +0900

    storeDetail - reservation

--- src/main/java/com/javaex/dao/StoreDetailDao.java
+++ src/main/java/com/javaex/dao/StoreDetailDao.java
@@ -1,5 +1,30 @@
 package com.javaex.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.OrderVo;
+
+@Repository
 public class StoreDetailDao {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<OrderVo> reservList() {
+		
+		List<OrderVo> rList = sqlSession.selectList("storeDetail.reservList");
+		
+		return rList;
+	}
+	
+	public OrderVo attend(OrderVo orderVo) {
+		
+		OrderVo attendVo = sqlSession.selectOne("storeDetail.attend", orderVo);
+		
+		return attendVo;
+	}
 }
