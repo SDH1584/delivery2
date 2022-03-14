6a2b5056a7d42f87193402be05370b4020b13467
KYS <kyseul513@hanmail.net>
2022-03-14 09:47:54 +0900

    orderjoin 수정

--- src/main/java/com/javaex/controller/StoreDetailController.java
+++ src/main/java/com/javaex/controller/StoreDetailController.java
@@ -39,10 +39,12 @@ public class StoreDetailController {
 			@PathVariable("storeNo") int storeNo, 
 			Model model) {
 
+		// 로그인 안되어 있는 경우 loginForm으로 이동
 		if (no == 0) {
 
 			return "user/loginForm";
 
+		// 로그인 되어있는 경우 p_order_no 존재하는지 확인.
 		} else {
 
 			OrderVo orderVo = new OrderVo();
@@ -53,16 +55,19 @@ public class StoreDetailController {
 			int pOrderNo = storeDetailService.attend(orderVo);
 			System.out.println("Controller.resultNo: " + pOrderNo);
 
+			//p_order_no가 없는 경우 해당 오더 참여페이지로 이동.
 			if (pOrderNo == 0) {
 				
 				model.addAttribute("orderVo",orderVo);
-				return "store-detail/orderJoin";
+				return "redirect:orderJoin?orderNo="+orderNo;
 
+			//p_order_no가 있는 경우 host인지 attendee인지 확인.
 			} else {
 
 				int attendVfy = storeDetailService.attendVfy(orderVo);
 				System.out.println("controller.attendVfy: " + attendVfy);
 
+				//attendVfy가 0, 즉 호스트인 경우 호스트의 오더 수정 페이지로 이동.
 				if (attendVfy == 0) {
 
 					Map<String, Object> orderInfoMap = storeDetailService.menuList(orderVo);
@@ -72,6 +77,7 @@ public class StoreDetailController {
 					
 					return "store-detail/order-change-host";
 
+				//attendVfy가 1, 즉 참가자인 경우 참가자의 오더 수정 페이지로 이동.
 				} else {
 
 					Map<String, Object> orderInfoMap = storeDetailService.menuList(orderVo);
@@ -86,12 +92,6 @@ public class StoreDetailController {
 		}
 	}
 
-
-	@RequestMapping("orderJoin")
-	public String orderJoin() {
-
-		return "store-detail/orderJoin";
-	}
 
 	@RequestMapping("description")
 	public String description() {
